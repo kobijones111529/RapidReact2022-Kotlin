7479bbd3deb1f02fd39a0fe1ade67590ae4774ef
Kobi Jones <kobijones@Kobis-MacBook-Pro.local>
2022-01-28 00:19:03 -0600

    Minor changes

--- src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
+++ src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
@@ -1,8 +1,11 @@
 package frc.robot.subsystems
 
+import edu.wpi.first.wpilibj.motorcontrol.Talon
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class Drivetrain() : SubsystemBase() {
+
+
     init {
 
     }
