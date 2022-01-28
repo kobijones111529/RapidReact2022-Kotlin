c4472815eb5af56b495364803ab7579be3177654
Kobi Jones <kobijonesmail@gmail.com>
2022-01-28 00:15:12 -0600

    Built Phoenix dep

--- src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
+++ src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
@@ -1,8 +1,11 @@
 package frc.robot.subsystems
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class Drivetrain() : SubsystemBase() {
+    val driveFrontLeft: WPI_TalonSRX = WPI_TalonSRX(0)
+
     init {
 
     }
