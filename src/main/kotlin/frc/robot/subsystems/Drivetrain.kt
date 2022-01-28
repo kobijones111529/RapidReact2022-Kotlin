965c337eae4e2668e70cb68e6f07507825bda5f3
Kobi Jones <kobijonesmail@gmail.com>
2022-01-28 00:23:05 -0600

    Quick fix

--- src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
+++ src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
@@ -3,6 +3,8 @@ package frc.robot.subsystems
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class Drivetrain() : SubsystemBase() {
+//    val driveFrontLeft: WPI_TalonSRX = WPI_TalonSRX(0)
+
     init {
 
     }
